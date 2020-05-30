fdff4935a880bb118bb9e8813706a25629035ce9
Yves <byiringiroyves127@gmail.com>
2020-05-30 19:07:13 +0200

    updates

--- rootFolder/settings.py
+++ rootFolder/settings.py
@@ -36,7 +36,7 @@ DEBUG = True
 
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ur-articles.herokuapp.com']
 # 'ur-articles.herokuapp.com'
 
 
@@ -143,16 +143,16 @@ WSGI_APPLICATION = 'rootFolder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': dj_database_url.config()
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 
