49c0e935a5741cea08585d73c719ec9f1a4e3917
Yves <byiringiroyves127@gmail.com>
2020-06-29 06:16:23 +0200

    styling email field on subscribe form

---
+++ static_base/js/custom.2af6d70d919d.js
@@ -0,0 +1,27 @@
+
+const myButton = document.getElementById("reply_btn");
+
+
+myButton.addEventListener("click", function(){
+
+    const replies = document.getElementById("all_replies");
+
+    if (replies.classList.contains('d-none')){
+        replies.classList.remove('d-none');
+        myButton.textContent = "Comment";
+
+    }
+    else{
+        replies.classList.add('d-none');
+        myButton.textContent = "Reply";
+        
+
+ 
+    }
+
+
+})
+
+
+
+
